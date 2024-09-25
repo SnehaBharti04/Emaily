bc413c0e8b300f94c0c83da4a74f951caae1f6cb
Sneha Bharti <Sneha.Bharti@einfochips.com>
2024-09-25 13:28:55 +0530

    My last commit to handle few errors

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,16 +1,16 @@
-const _ = require('lodash');
-const { Path } = require('path-parser');
-const { URL } = require('url');
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
-const requireCredits = require('../middlewares/requireCredits');
-const Mailer = require('../services/Mailer');
-const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+const _ = require("lodash");
+const { Path } = require("path-parser");
+const { URL } = require("url");
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const requireCredits = require("../middlewares/requireCredits");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
-const Survey = mongoose.model('surveys');
+const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
-  app.get('/api/surveys', requireLogin, async (req, res) => {
+  app.get("/api/surveys", requireLogin, async (req, res) => {
     const surveys = await Survey.find({ _user: req.user.id }).select({
       recipients: false,
     });
@@ -18,22 +18,28 @@ module.exports = (app) => {
     res.send(surveys);
   });
 
-  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
-    res.send('Thanks for voting!');
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
+    res.send("Thanks for voting!");
   });
 
-  app.post('/api/surveys/webhooks', (req, res) => {
-    const p = new Path('/api/surveys/:surveyId/:choice');
+  app.post("/api/surveys/webhooks", (req, res) => {
+    console.log("Webhook hit");
+    const p = new Path("/api/surveys/:surveyId/:choice");
 
     _.chain(req.body)
       .map(({ email, url }) => {
         const match = p.test(new URL(url).pathname);
+        console.log("Matching URL:", match);
         if (match) {
-          return { email, surveyId: match.surveyId, choice: match.choice };
+          return {
+            email,
+            surveyId: match.surveyId,
+            choice: match.choice,
+          };
         }
       })
       .compact()
-      .uniqBy('email', 'surveyId')
+      .uniqBy("email", "surveyId")
       .each(({ surveyId, email, choice }) => {
         Survey.updateOne(
           {
@@ -44,7 +50,7 @@ module.exports = (app) => {
           },
           {
             $inc: { [choice]: 1 },
-            $set: { 'recipients.$.responded': true },
+            $set: { "recipients.$.responded": true },
             lastResponded: new Date(),
           }
         ).exec();
@@ -54,7 +60,7 @@ module.exports = (app) => {
     res.send({});
   });
 
-  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
@@ -62,7 +68,7 @@ module.exports = (app) => {
       subject,
       body,
       recipients: recipients
-        .split(',')
+        .split(",")
         .map((email) => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now(),
